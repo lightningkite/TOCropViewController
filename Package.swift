943cb3a10a10f6728c305692c313b291cd861abc
Shane Thompson <shane@meosphere.com>
2025-05-01 10:55:03 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,19 +3,10 @@
 
 import PackageDescription
 
-var platforms: [SupportedPlatform] {
-    #if compiler(<5.3)
-        return [.iOS(.v8)]
-    #else
-        // Xcode 12 (which ships with Swift 5.3) drops support for iOS 8
-        return[.iOS(.v9)]
-    #endif
-}
-
 let package = Package(
     name: "TOCropViewController",
     defaultLocalization: "en",
-    platforms: platforms,
+    platforms: [.ios(.v12)],
     products: [
         .library(
             name: "TOCropViewController",
